c3b8442dc2ae17de9aa0aef680b7380b1feb8e50
Krutika529 <dhakate189@gmail.com>
2026-02-26 21:59:31 +0800

    created simple stage for jira

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './app.css';
+const App = () => {
+  return (
+    <div className='app'>
+      <header className='app_header'> Header Section </header>
+      <main className='app_main'>
+        <section className='task_column'> Section 1 </section>
+        <section className='task_column'> Section 2 </section>
+        <section className='task_column'> Section 3 </section>
+        <section className='task_column'> Section 4 </section>
+
+      </main>
+    </div>
+
+  )
+}
+
+export default App
+
